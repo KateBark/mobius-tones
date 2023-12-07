76bc70625be42e14a7182fdc92797bb838e6ac7f
Kate Bark <barkkate@gmail.com>
2023-12-07 15:24:35 -0500

    continue set-up. add components and pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,18 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import './styles./App.scss';
+import { BrowserRouter, Routes, Route, useParams } from "react-router-dom";
+import './styles/App.scss';
+import Header from "./components/header/Header";
 
 function App() {
+  const { myvids, mystuff } = useParams();
   return (
     <BrowserRouter>
       <Header />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/myvids" element={<MyVids />} />
-        <Route path="/mystuff" element={<MyStuff />} />
+        {/* <Route path="/" element={<Home />} /> */}
+        {/* <Route path="/:myvids" element={<MyVids />} /> */}
+        {/* <Route path="/:mystuff" element={<MyStuff />} /> */}
       </Routes>
-
+      {/* <Footer /> */}
     </BrowserRouter>
   );
 }
---
+++ client/src/pages/home/Home.js
@@ -0,0 +1,11 @@
+import "./Home.scss";
+
+function Home() {
+  return (
+    <main>
+
+    </main>
+  );
+}
+
+export default Home;
